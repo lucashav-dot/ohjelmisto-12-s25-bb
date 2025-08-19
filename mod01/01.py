ecc4cdaef1cccc00086d2ffe6d47759def73189a
Lucashav-dot <lucashav@metropolia.fi>
2025-08-19 14:46:08 +0300

    add

---
+++ mod01/01.py
@@ -0,0 +1,15 @@
+#kommentti
+print("Terve!")
+print("Terve!")
+print("Terve!")
+print("Terve!")
+print("hei, Lucas \n Havanko")
+
+print("======================================")
+#yksinkertainen muuttuja
+nimi = "Lucas"
+print(nimi)
+print("======================================")
+#syöte, eli tietoa käyttäjältä
+nimi2 = input("anna nimesi")
+print(nimi2)
